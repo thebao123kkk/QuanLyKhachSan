a2ce46a8f0c3d18ad3471fcde857c0183174899e
Tiến Dũng <tienpham3005td@gmail.com>
2025-11-13 00:25:33 +0700

    Sua du lieu cho inset luc loi

--- SQL/QLKS.sql
+++ SQL/QLKS.sql
@@ -280,16 +280,10 @@ INSERT INTO NhanVien VALUES
 ('NV03', N'Phạm Thu Thảo', N'Kế toán', N'Nữ', '1995-12-15', '0909888777', 'thao.pham@hotel.vn', N'Hà Nội', 'VT04', N'Đang làm', GETDATE());
 
 INSERT INTO TaiKhoanHeThong VALUES
-<<<<<<< HEAD
 ('TK01', 'admin', 'E10ADC3949BA59ABBE56E057F20F883E', N'Quản lý', N'Hoạt động', 'mai.tran@hotel.vn', 'NV01', GETDATE()),
 ('TK02', 'letuan', 'E10ADC3949BA59ABBE56E057F20F883E', N'Lễ tân', N'Hoạt động', 'tuan.le@hotel.vn', 'NV02', GETDATE()),
 ('TK03', 'thaopham', 'E10ADC3949BA59ABBE56E057F20F883E', N'Kế toán', N'Hoạt động', 'thao.pham@hotel.vn', 'NV03', GETDATE());
-=======
-('TK01', 'admin', 'E10ADC3949BA59ABBE56E057F20F883E', 'Quản lý', N'Hoạt động', 'mai.tran@hotel.vn', 'NV01', GETDATE(), 0),
-('TK02', 'letuan', 'E10ADC3949BA59ABBE56E057F20F883E', 'Lễ tân', N'Hoạt động', 'tuan.le@hotel.vn', 'NV02', GETDATE()), 0,
-('TK03', 'thaopham', 'E10ADC3949BA59ABBE56E057F20F883E', 'Kế toán', N'Hoạt động', 'thao.pham@hotel.vn', 'NV03', GETDATE(), 0);
 
->>>>>>> 1b57b4e6459fa4f3980c7dcbb225f309e07c46f2
 
 -- QUẢN LÝ PHÒNG
 -- Nhóm loại phòng
